b611ee42334c8ddc7a699adacaa280f7713a7450
Manoel Marques <manoel@us.ibm.com>
2019-01-07 11:05:10 -0500

    Update constructor

--- mock_chemistry_drivers/mockdriver/mockdriver.py
+++ mock_chemistry_drivers/mockdriver/mockdriver.py
@@ -16,7 +16,10 @@
 # =============================================================================
 
 from qiskit_chemistry.drivers import BaseDriver
-from qiskit_chemistry import QMolecule
+from qiskit_chemistry import QMolecule, QiskitChemistryError
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class MockDriver(BaseDriver):
@@ -33,8 +36,38 @@ class MockDriver(BaseDriver):
         }
     }
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, config):
+        """
+        Initializer
+        Args:
+            config (str or list): driver configuration
+        """
+        if not isinstance(config, list) and not isinstance(config, str):
+            raise QiskitChemistryError("Invalid input for MockDriver Driver '{}'".format(config))
 
-    def run(self, section):
+        if isinstance(config, list):
+            config = '\n'.join(config)
+
+        super().__init__()
+        self._config = config
+
+    @classmethod
+    def init_from_input(cls, section):
+        """
+        Initialize via section dictionary.
+
+        Args:
+            params (dict): section dictionary
+
+        Returns:
+            Driver: Driver object
+        """
+        if 'data' not in section:
+            raise QiskitChemistryError('Missing data section')
+
+        kwargs = {'config': section['data']}
+        logger.debug('init_from_input: {}'.format(kwargs))
+        return cls(**kwargs)
+
+    def run(self):
         return QMolecule()
